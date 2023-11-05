23a5d676936efc4bd8b2776a90937532c6d314e3
akyamadesu1 <akyama.megumi@gmail.com>
2023-11-05 18:09:44 -0300

    alteraÃcoes

--- src/testesCalculadora.java
+++ src/testesCalculadora.java
@@ -6,25 +6,25 @@ public class testesCalculadora {
     public void testeSoma(){
         Calculadora calc = new Calculadora();
         int resultado = calc.soma(2,2);
-        System.out.println("RESULTADO DA SOMA: " + resultado);
+        System.out.println("Resultado da Soma: " + resultado);
     }
 
     public void testeSubtracao(){
         Calculadora calc = new Calculadora();
         int resultado = calc.subtracao(2,2);
-        System.out.println("RESULTADO DA SUBTRACAO: " + resultado);
+        System.out.println("Resultado da Subtração: " + resultado);
     }
 
     public void testeMultiplicacao(){
         Calculadora calc = new Calculadora();
         int resultado = calc.multiplicacao(2,2);
-        System.out.println("RESULTADO DA MULTIPLICACAO: " + resultado);
+        System.out.println("Resultado da Multiplicação: " + resultado);
     }
 
     public void testeDivisao(){
         Calculadora calc = new Calculadora();
         int resultado = calc.divisao(2,2);
-        System.out.println("RESULTADO DA DIVISAO: " + resultado);
+        System.out.println("Resultado da Divisão: " + resultado);
     }
 
 
