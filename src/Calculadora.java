f61162d6f8283c04969c1c5f2c968fd717b46a8a
akyamadesu1 <akyama.megumi@gmail.com>
2023-11-06 11:59:22 -0300

    documentacao

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,22 +1,50 @@
+/**
+ * A classe Calculadora contém métodos para realizar operações matemáticas básicas.
+ * @version 1.0
+ * @author Megumi
+ */
 public class Calculadora {
 
-
-    //Recebe 2 inteiros e retorna a soma deles
+    /**
+     * método de adição que recebe dois números inteiros, soma e retorna o resultado.
+     *
+     * @param a - primeiro número.
+     * @param b - segundo número.
+     * @return - retorna a soma de a e b.
+     */
     public int soma(int a, int b){
         return a + b;
     }
 
-    //Recebe 2 inteiros e retorna a subtracao deles
+    /**
+     * método de subtração que recebe dois números inteiros, subtrai e retorna o resultado.
+     *
+     * @param a - primeiro número.
+     * @param b - segundo número.
+     * @return retorna a subtração entre a e b.
+     */
     public int subtracao(int a, int b){
         return a - b;
     }
 
-    //Recebe 2 inteiros e retorna a multiplicacao deles
+    /**
+     * método de multiplicação que recebe dois números inteiros, multiplica e retorna o resultado.
+     *
+     * @param a O primeiro número.
+     * @param b O segundo número.
+     * @return retorna a multiplicação entre a e b.
+     */
     public int multiplicacao(int a, int b){
         return a * b;
     }
 
-    //Recebe 2 inteiros e retorna a divisao deles
+    /**
+     * método de divisão que recebe dois números inteiros, divide e retorna o resultado.
+     *
+     * @param a - primeiro número (dividendo).
+     * @param b - segundo número (divisor).
+     * @return - retorna a divisão de a por b.
+     */
     public int divisao(int a, int b){
         return a / b;
     }
